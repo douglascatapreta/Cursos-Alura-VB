dcc679a51864c7a570fcf7b4c250038039a2d4f9
Douglas Cata Preta <douglascatapreta@gmail.com>
2020-06-15 20:46:02 -0300

    Inclusão do projeto desenvolvido durante o curso "VB.NET com Windows Forms Parte 1: Primeiros passos".

---
+++ VB.NET Com Windows Forms Parte 1 - Primeiros Passos/Aprendendo VB/4 - Controle de Fluxo/Frm_TelaPrincipal_04.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_TelaPrincipal_04
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btm_Principal = New System.Windows.Forms.Button()
+        Me.Lbl_NomeProjeto = New System.Windows.Forms.Label()
+        Me.Lbl_Idade = New System.Windows.Forms.Label()
+        Me.Txt_Idade = New System.Windows.Forms.TextBox()
+        Me.Txt_Resultado = New System.Windows.Forms.TextBox()
+        Me.Lbl_Resultado = New System.Windows.Forms.Label()
+        Me.Grp_Pais = New System.Windows.Forms.GroupBox()
+        Me.Rdb_Sim = New System.Windows.Forms.RadioButton()
+        Me.Rdb_Nao = New System.Windows.Forms.RadioButton()
+        Me.Grp_Pais.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Btm_Principal
+        '
+        Me.Btm_Principal.Location = New System.Drawing.Point(114, 113)
+        Me.Btm_Principal.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Btm_Principal.Name = "Btm_Principal"
+        Me.Btm_Principal.Size = New System.Drawing.Size(565, 97)
+        Me.Btm_Principal.TabIndex = 0
+        Me.Btm_Principal.Text = "Clique aqui"
+        Me.Btm_Principal.UseVisualStyleBackColor = True
+        '
+        'Lbl_NomeProjeto
+        '
+        Me.Lbl_NomeProjeto.AutoSize = True
+        Me.Lbl_NomeProjeto.Location = New System.Drawing.Point(16, 11)
+        Me.Lbl_NomeProjeto.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Lbl_NomeProjeto.Name = "Lbl_NomeProjeto"
+        Me.Lbl_NomeProjeto.Size = New System.Drawing.Size(130, 17)
+        Me.Lbl_NomeProjeto.TabIndex = 1
+        Me.Lbl_NomeProjeto.Text = "1 - Primeiro Projeto"
+        '
+        'Lbl_Idade
+        '
+        Me.Lbl_Idade.AutoSize = True
+        Me.Lbl_Idade.Location = New System.Drawing.Point(16, 44)
+        Me.Lbl_Idade.Name = "Lbl_Idade"
+        Me.Lbl_Idade.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_Idade.TabIndex = 2
+        Me.Lbl_Idade.Text = "Label1"
+        '
+        'Txt_Idade
+        '
+        Me.Txt_Idade.Location = New System.Drawing.Point(19, 64)
+        Me.Txt_Idade.Name = "Txt_Idade"
+        Me.Txt_Idade.Size = New System.Drawing.Size(247, 22)
+        Me.Txt_Idade.TabIndex = 3
+        '
+        'Txt_Resultado
+        '
+        Me.Txt_Resultado.Location = New System.Drawing.Point(19, 241)
+        Me.Txt_Resultado.Name = "Txt_Resultado"
+        Me.Txt_Resultado.Size = New System.Drawing.Size(769, 22)
+        Me.Txt_Resultado.TabIndex = 5
+        '
+        'Lbl_Resultado
+        '
+        Me.Lbl_Resultado.AutoSize = True
+        Me.Lbl_Resultado.Location = New System.Drawing.Point(16, 221)
+        Me.Lbl_Resultado.Name = "Lbl_Resultado"
+        Me.Lbl_Resultado.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_Resultado.TabIndex = 4
+        Me.Lbl_Resultado.Text = "Label1"
+        '
+        'Grp_Pais
+        '
+        Me.Grp_Pais.Controls.Add(Me.Rdb_Nao)
+        Me.Grp_Pais.Controls.Add(Me.Rdb_Sim)
+        Me.Grp_Pais.Location = New System.Drawing.Point(440, 32)
+        Me.Grp_Pais.Name = "Grp_Pais"
+        Me.Grp_Pais.Size = New System.Drawing.Size(348, 66)
+        Me.Grp_Pais.TabIndex = 6
+        Me.Grp_Pais.TabStop = False
+        Me.Grp_Pais.Text = "GroupBox1"
+        '
+        'Rdb_Sim
+        '
+        Me.Rdb_Sim.AutoSize = True
+        Me.Rdb_Sim.Location = New System.Drawing.Point(49, 24)
+        Me.Rdb_Sim.Name = "Rdb_Sim"
+        Me.Rdb_Sim.Size = New System.Drawing.Size(115, 21)
+        Me.Rdb_Sim.TabIndex = 0
+        Me.Rdb_Sim.TabStop = True
+        Me.Rdb_Sim.Text = "RadioButton1"
+        Me.Rdb_Sim.UseVisualStyleBackColor = True
+        '
+        'Rdb_Nao
+        '
+        Me.Rdb_Nao.AutoSize = True
+        Me.Rdb_Nao.Location = New System.Drawing.Point(192, 24)
+        Me.Rdb_Nao.Name = "Rdb_Nao"
+        Me.Rdb_Nao.Size = New System.Drawing.Size(115, 21)
+        Me.Rdb_Nao.TabIndex = 1
+        Me.Rdb_Nao.TabStop = True
+        Me.Rdb_Nao.Text = "RadioButton1"
+        Me.Rdb_Nao.UseVisualStyleBackColor = True
+        '
+        'Frm_TelaPricipal_04
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Grp_Pais)
+        Me.Controls.Add(Me.Txt_Resultado)
+        Me.Controls.Add(Me.Lbl_Resultado)
+        Me.Controls.Add(Me.Txt_Idade)
+        Me.Controls.Add(Me.Lbl_Idade)
+        Me.Controls.Add(Me.Lbl_NomeProjeto)
+        Me.Controls.Add(Me.Btm_Principal)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Frm_TelaPricipal_04"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplicação Alô Mundo"
+        Me.Grp_Pais.ResumeLayout(False)
+        Me.Grp_Pais.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btm_Principal As Button
+    Friend WithEvents Lbl_NomeProjeto As Label
+    Friend WithEvents Lbl_Idade As Label
+    Friend WithEvents Txt_Idade As TextBox
+    Friend WithEvents Txt_Resultado As TextBox
+    Friend WithEvents Lbl_Resultado As Label
+    Friend WithEvents Grp_Pais As GroupBox
+    Friend WithEvents Rdb_Sim As RadioButton
+    Friend WithEvents Rdb_Nao As RadioButton
+End Class
---
+++ VB.NET Com Windows Forms Parte 1 - Primeiros Passos/Aprendendo VB/5 - Laços de Repetição/Frm_TelaPrincipal_05.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_TelaPrincipal_05
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btm_While = New System.Windows.Forms.Button()
+        Me.Lbl_NomeProjeto = New System.Windows.Forms.Label()
+        Me.Lbl_Principal = New System.Windows.Forms.Label()
+        Me.Lbl_Rendimento = New System.Windows.Forms.Label()
+        Me.Txt_Principal = New System.Windows.Forms.TextBox()
+        Me.Txt_Rendimento = New System.Windows.Forms.TextBox()
+        Me.Lbl_Periodos = New System.Windows.Forms.Label()
+        Me.Txt_Periodos = New System.Windows.Forms.TextBox()
+        Me.Txt_Extrato_While = New System.Windows.Forms.TextBox()
+        Me.Btm_For = New System.Windows.Forms.Button()
+        Me.Txt_Extrato_For = New System.Windows.Forms.TextBox()
+        Me.Grp_Grupo1 = New System.Windows.Forms.GroupBox()
+        Me.Grp_Grupo2 = New System.Windows.Forms.GroupBox()
+        Me.Lbl_Principal2 = New System.Windows.Forms.Label()
+        Me.Lbl_Rendimento2 = New System.Windows.Forms.Label()
+        Me.Lbl_Anos = New System.Windows.Forms.Label()
+        Me.Lbl_AcrescimoRendimento = New System.Windows.Forms.Label()
+        Me.Txt_Principal2 = New System.Windows.Forms.TextBox()
+        Me.Txt_Rendimento2 = New System.Windows.Forms.TextBox()
+        Me.Txt_Anos = New System.Windows.Forms.TextBox()
+        Me.Txt_AcrescimoRendimento = New System.Windows.Forms.TextBox()
+        Me.Btm_Calcula = New System.Windows.Forms.Button()
+        Me.Txt_Resultado = New System.Windows.Forms.TextBox()
+        Me.Lbl_Limite = New System.Windows.Forms.Label()
+        Me.Txt_Limite = New System.Windows.Forms.TextBox()
+        Me.Grp_Grupo1.SuspendLayout()
+        Me.Grp_Grupo2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Btm_While
+        '
+        Me.Btm_While.Location = New System.Drawing.Point(9, 65)
+        Me.Btm_While.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Btm_While.Name = "Btm_While"
+        Me.Btm_While.Size = New System.Drawing.Size(396, 52)
+        Me.Btm_While.TabIndex = 0
+        Me.Btm_While.Text = "Botão principal"
+        Me.Btm_While.UseVisualStyleBackColor = True
+        '
+        'Lbl_NomeProjeto
+        '
+        Me.Lbl_NomeProjeto.AutoSize = True
+        Me.Lbl_NomeProjeto.Location = New System.Drawing.Point(16, 11)
+        Me.Lbl_NomeProjeto.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Lbl_NomeProjeto.Name = "Lbl_NomeProjeto"
+        Me.Lbl_NomeProjeto.Size = New System.Drawing.Size(113, 17)
+        Me.Lbl_NomeProjeto.TabIndex = 1
+        Me.Lbl_NomeProjeto.Text = "Nome do projeto"
+        '
+        'Lbl_Principal
+        '
+        Me.Lbl_Principal.AutoSize = True
+        Me.Lbl_Principal.Location = New System.Drawing.Point(6, 18)
+        Me.Lbl_Principal.Name = "Lbl_Principal"
+        Me.Lbl_Principal.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_Principal.TabIndex = 2
+        Me.Lbl_Principal.Text = "Label1"
+        '
+        'Lbl_Rendimento
+        '
+        Me.Lbl_Rendimento.AutoSize = True
+        Me.Lbl_Rendimento.Location = New System.Drawing.Point(318, 18)
+        Me.Lbl_Rendimento.Name = "Lbl_Rendimento"
+        Me.Lbl_Rendimento.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_Rendimento.TabIndex = 3
+        Me.Lbl_Rendimento.Text = "Label2"
+        '
+        'Txt_Principal
+        '
+        Me.Txt_Principal.Location = New System.Drawing.Point(9, 38)
+        Me.Txt_Principal.Name = "Txt_Principal"
+        Me.Txt_Principal.Size = New System.Drawing.Size(189, 22)
+        Me.Txt_Principal.TabIndex = 4
+        '
+        'Txt_Rendimento
+        '
+        Me.Txt_Rendimento.Location = New System.Drawing.Point(321, 38)
+        Me.Txt_Rendimento.Name = "Txt_Rendimento"
+        Me.Txt_Rendimento.Size = New System.Drawing.Size(189, 22)
+        Me.Txt_Rendimento.TabIndex = 5
+        '
+        'Lbl_Periodos
+        '
+        Me.Lbl_Periodos.AutoSize = True
+        Me.Lbl_Periodos.Location = New System.Drawing.Point(615, 18)
+        Me.Lbl_Periodos.Name = "Lbl_Periodos"
+        Me.Lbl_Periodos.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_Periodos.TabIndex = 6
+        Me.Lbl_Periodos.Text = "Label3"
+        '
+        'Txt_Periodos
+        '
+        Me.Txt_Periodos.Location = New System.Drawing.Point(618, 38)
+        Me.Txt_Periodos.Name = "Txt_Periodos"
+        Me.Txt_Periodos.Size = New System.Drawing.Size(189, 22)
+        Me.Txt_Periodos.TabIndex = 7
+        '
+        'Txt_Extrato_While
+        '
+        Me.Txt_Extrato_While.Location = New System.Drawing.Point(9, 122)
+        Me.Txt_Extrato_While.Multiline = True
+        Me.Txt_Extrato_While.Name = "Txt_Extrato_While"
+        Me.Txt_Extrato_While.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Txt_Extrato_While.Size = New System.Drawing.Size(396, 296)
+        Me.Txt_Extrato_While.TabIndex = 8
+        '
+        'Btm_For
+        '
+        Me.Btm_For.Location = New System.Drawing.Point(411, 65)
+        Me.Btm_For.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Btm_For.Name = "Btm_For"
+        Me.Btm_For.Size = New System.Drawing.Size(396, 52)
+        Me.Btm_For.TabIndex = 9
+        Me.Btm_For.Text = "Botão principal"
+        Me.Btm_For.UseVisualStyleBackColor = True
+        '
+        'Txt_Extrato_For
+        '
+        Me.Txt_Extrato_For.Location = New System.Drawing.Point(411, 122)
+        Me.Txt_Extrato_For.Multiline = True
+        Me.Txt_Extrato_For.Name = "Txt_Extrato_For"
+        Me.Txt_Extrato_For.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Txt_Extrato_For.Size = New System.Drawing.Size(396, 296)
+        Me.Txt_Extrato_For.TabIndex = 10
+        '
+        'Grp_Grupo1
+        '
+        Me.Grp_Grupo1.Controls.Add(Me.Lbl_Principal)
+        Me.Grp_Grupo1.Controls.Add(Me.Txt_Extrato_For)
+        Me.Grp_Grupo1.Controls.Add(Me.Btm_While)
+        Me.Grp_Grupo1.Controls.Add(Me.Btm_For)
+        Me.Grp_Grupo1.Controls.Add(Me.Lbl_Rendimento)
+        Me.Grp_Grupo1.Controls.Add(Me.Txt_Extrato_While)
+        Me.Grp_Grupo1.Controls.Add(Me.Txt_Principal)
+        Me.Grp_Grupo1.Controls.Add(Me.Txt_Periodos)
+        Me.Grp_Grupo1.Controls.Add(Me.Txt_Rendimento)
+        Me.Grp_Grupo1.Controls.Add(Me.Lbl_Periodos)
+        Me.Grp_Grupo1.Location = New System.Drawing.Point(19, 44)
+        Me.Grp_Grupo1.Name = "Grp_Grupo1"
+        Me.Grp_Grupo1.Size = New System.Drawing.Size(817, 429)
+        Me.Grp_Grupo1.TabIndex = 11
+        Me.Grp_Grupo1.TabStop = False
+        Me.Grp_Grupo1.Text = "GroupBox1"
+        '
+        'Grp_Grupo2
+        '
+        Me.Grp_Grupo2.Controls.Add(Me.Txt_Limite)
+        Me.Grp_Grupo2.Controls.Add(Me.Lbl_Limite)
+        Me.Grp_Grupo2.Controls.Add(Me.Txt_Resultado)
+        Me.Grp_Grupo2.Controls.Add(Me.Btm_Calcula)
+        Me.Grp_Grupo2.Controls.Add(Me.Txt_AcrescimoRendimento)
+        Me.Grp_Grupo2.Controls.Add(Me.Txt_Anos)
+        Me.Grp_Grupo2.Controls.Add(Me.Txt_Rendimento2)
+        Me.Grp_Grupo2.Controls.Add(Me.Txt_Principal2)
+        Me.Grp_Grupo2.Controls.Add(Me.Lbl_AcrescimoRendimento)
+        Me.Grp_Grupo2.Controls.Add(Me.Lbl_Anos)
+        Me.Grp_Grupo2.Controls.Add(Me.Lbl_Rendimento2)
+        Me.Grp_Grupo2.Controls.Add(Me.Lbl_Principal2)
+        Me.Grp_Grupo2.Location = New System.Drawing.Point(852, 44)
+        Me.Grp_Grupo2.Name = "Grp_Grupo2"
+        Me.Grp_Grupo2.Size = New System.Drawing.Size(817, 429)
+        Me.Grp_Grupo2.TabIndex = 12
+        Me.Grp_Grupo2.TabStop = False
+        Me.Grp_Grupo2.Text = "GroupBox2"
+        '
+        'Lbl_Principal2
+        '
+        Me.Lbl_Principal2.AutoSize = True
+        Me.Lbl_Principal2.Location = New System.Drawing.Point(26, 18)
+        Me.Lbl_Principal2.Name = "Lbl_Principal2"
+        Me.Lbl_Principal2.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_Principal2.TabIndex = 0
+        Me.Lbl_Principal2.Text = "Label1"
+        '
+        'Lbl_Rendimento2
+        '
+        Me.Lbl_Rendimento2.AutoSize = True
+        Me.Lbl_Rendimento2.Location = New System.Drawing.Point(201, 17)
+        Me.Lbl_Rendimento2.Name = "Lbl_Rendimento2"
+        Me.Lbl_Rendimento2.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_Rendimento2.TabIndex = 1
+        Me.Lbl_Rendimento2.Text = "Label2"
+        '
+        'Lbl_Anos
+        '
+        Me.Lbl_Anos.AutoSize = True
+        Me.Lbl_Anos.Location = New System.Drawing.Point(368, 18)
+        Me.Lbl_Anos.Name = "Lbl_Anos"
+        Me.Lbl_Anos.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_Anos.TabIndex = 2
+        Me.Lbl_Anos.Text = "Label3"
+        '
+        'Lbl_AcrescimoRendimento
+        '
+        Me.Lbl_AcrescimoRendimento.AutoSize = True
+        Me.Lbl_AcrescimoRendimento.Location = New System.Drawing.Point(525, 18)
+        Me.Lbl_AcrescimoRendimento.Name = "Lbl_AcrescimoRendimento"
+        Me.Lbl_AcrescimoRendimento.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_AcrescimoRendimento.TabIndex = 3
+        Me.Lbl_AcrescimoRendimento.Text = "Label4"
+        '
+        'Txt_Principal2
+        '
+        Me.Txt_Principal2.Location = New System.Drawing.Point(29, 38)
+        Me.Txt_Principal2.Name = "Txt_Principal2"
+        Me.Txt_Principal2.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_Principal2.TabIndex = 4
+        '
+        'Txt_Rendimento2
+        '
+        Me.Txt_Rendimento2.Location = New System.Drawing.Point(204, 38)
+        Me.Txt_Rendimento2.Name = "Txt_Rendimento2"
+        Me.Txt_Rendimento2.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_Rendimento2.TabIndex = 5
+        '
+        'Txt_Anos
+        '
+        Me.Txt_Anos.Location = New System.Drawing.Point(371, 38)
+        Me.Txt_Anos.Name = "Txt_Anos"
+        Me.Txt_Anos.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_Anos.TabIndex = 6
+        '
+        'Txt_AcrescimoRendimento
+        '
+        Me.Txt_AcrescimoRendimento.Location = New System.Drawing.Point(528, 38)
+        Me.Txt_AcrescimoRendimento.Name = "Txt_AcrescimoRendimento"
+        Me.Txt_AcrescimoRendimento.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_AcrescimoRendimento.TabIndex = 7
+        '
+        'Btm_Calcula
+        '
+        Me.Btm_Calcula.Location = New System.Drawing.Point(29, 65)
+        Me.Btm_Calcula.Name = "Btm_Calcula"
+        Me.Btm_Calcula.Size = New System.Drawing.Size(757, 50)
+        Me.Btm_Calcula.TabIndex = 8
+        Me.Btm_Calcula.Text = "Button1"
+        Me.Btm_Calcula.UseVisualStyleBackColor = True
+        '
+        'Txt_Resultado
+        '
+        Me.Txt_Resultado.Location = New System.Drawing.Point(29, 123)
+        Me.Txt_Resultado.Multiline = True
+        Me.Txt_Resultado.Name = "Txt_Resultado"
+        Me.Txt_Resultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Txt_Resultado.Size = New System.Drawing.Size(757, 296)
+        Me.Txt_Resultado.TabIndex = 11
+        '
+        'Lbl_Limite
+        '
+        Me.Lbl_Limite.AutoSize = True
+        Me.Lbl_Limite.Location = New System.Drawing.Point(683, 17)
+        Me.Lbl_Limite.Name = "Lbl_Limite"
+        Me.Lbl_Limite.Size = New System.Drawing.Size(51, 17)
+        Me.Lbl_Limite.TabIndex = 12
+        Me.Lbl_Limite.Text = "Label5"
+        '
+        'Txt_Limite
+        '
+        Me.Txt_Limite.Location = New System.Drawing.Point(686, 37)
+        Me.Txt_Limite.Name = "Txt_Limite"
+        Me.Txt_Limite.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_Limite.TabIndex = 13
+        '
+        'Frm_TelaPrincipal_05
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1694, 504)
+        Me.Controls.Add(Me.Grp_Grupo2)
+        Me.Controls.Add(Me.Grp_Grupo1)
+        Me.Controls.Add(Me.Lbl_NomeProjeto)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Frm_TelaPrincipal_05"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplicação Alô Mundo"
+        Me.Grp_Grupo1.ResumeLayout(False)
+        Me.Grp_Grupo1.PerformLayout()
+        Me.Grp_Grupo2.ResumeLayout(False)
+        Me.Grp_Grupo2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btm_While As Button
+    Friend WithEvents Lbl_NomeProjeto As Label
+    Friend WithEvents Lbl_Principal As Label
+    Friend WithEvents Lbl_Rendimento As Label
+    Friend WithEvents Txt_Principal As TextBox
+    Friend WithEvents Txt_Rendimento As TextBox
+    Friend WithEvents Lbl_Periodos As Label
+    Friend WithEvents Txt_Periodos As TextBox
+    Friend WithEvents Txt_Extrato_While As TextBox
+    Friend WithEvents Btm_For As Button
+    Friend WithEvents Txt_Extrato_For As TextBox
+    Friend WithEvents Grp_Grupo1 As GroupBox
+    Friend WithEvents Grp_Grupo2 As GroupBox
+    Friend WithEvents Lbl_AcrescimoRendimento As Label
+    Friend WithEvents Lbl_Anos As Label
+    Friend WithEvents Lbl_Rendimento2 As Label
+    Friend WithEvents Lbl_Principal2 As Label
+    Friend WithEvents Txt_AcrescimoRendimento As TextBox
+    Friend WithEvents Txt_Anos As TextBox
+    Friend WithEvents Txt_Rendimento2 As TextBox
+    Friend WithEvents Txt_Principal2 As TextBox
+    Friend WithEvents Btm_Calcula As Button
+    Friend WithEvents Txt_Resultado As TextBox
+    Friend WithEvents Txt_Limite As TextBox
+    Friend WithEvents Lbl_Limite As Label
+End Class
