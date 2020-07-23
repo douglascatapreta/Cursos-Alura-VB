5bd34dffc08c7cfe2fb4191f5157f1cb4a6314c4
Douglas Cata Preta <douglascatapreta@gmail.com>
2020-07-23 14:13:52 -0300

    Inclusão do projeto desenvolvido durante o curso "VB.NET com Windows Forms Parte 10: Entrada e saída (I/O) com streams".

---
+++ VB.NET com Windows Forms Parte 10 - Entrada e Saída (IO) com Streams/ByteBank/ByteBank.SistemaAgencia/Frm_EntradaSaida.vb
@@ -0,0 +1,151 @@
+﻿Imports System.IO
+Imports System.Text
+Imports ByteBank.Bibliotecas.Classes.Clientes
+
+Public Class Frm_EntradaSaida
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+
+        Dim EnderecoDoArquivo As String = "temp.txt"
+
+        Using FluxoDoArquivo As New FileStream(EnderecoDoArquivo, FileMode.Open)
+            Dim Buffer = New Byte(1024) {}
+            Dim NumeroBytesLidos As Integer = -1
+
+            While NumeroBytesLidos <> 0
+                NumeroBytesLidos = FluxoDoArquivo.Read(Buffer, 0, 1024)
+                If NumeroBytesLidos <> 0 Then
+                    TextBox1.AppendText(EscreverBuffer(Buffer, NumeroBytesLidos))
+                    TextBox2.AppendText(EscreverBufferUTF(Buffer, NumeroBytesLidos))
+                End If
+            End While
+        End Using
+    End Sub
+
+    Function EscreverBuffer(Buffer As Byte(), NumeroBytesLidos As Integer) As String
+
+        Dim vRetorno As String = ""
+        For i As Integer = 0 To NumeroBytesLidos - 1
+            vRetorno += Buffer(i).ToString + " "
+        Next
+
+        Return vRetorno
+
+    End Function
+
+    Function EscreverBufferUTF(Buffer As Byte(), NumeroBytesLidos As Integer) As String
+
+        Dim vRetorno As String
+        Dim UTF As New UTF8Encoding()
+        vRetorno = UTF.GetString(Buffer, 0, NumeroBytesLidos)
+
+        Return vRetorno
+
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+
+        Using FluxoDoArquivo As New FileStream("temp.csv", FileMode.Open)
+            Using Leitor As New StreamReader(FluxoDoArquivo, Encoding.UTF8)
+                While Not Leitor.EndOfStream()
+                    Dim Linha As String = Leitor.ReadLine()
+                    Dim CC As ContaCorrente = CriarContaCorrente(Linha)
+                    TextBox1.AppendText(Linha + vbCrLf)
+                    TextBox2.AppendText(CC.ToString + vbCrLf)
+                End While
+            End Using
+        End Using
+
+    End Sub
+
+    Function CriarContaCorrente(Linha As String) As ContaCorrente
+
+        Dim VetorLinha() As String = Linha.Split(";")
+
+        Dim Cliente As New Cliente() With {
+            .cpf = VetorLinha(3),
+            .nome = VetorLinha(4)
+        }
+
+        Dim Conta As New ContaCorrente(VetorLinha(0), VetorLinha(1)) With {
+            .titular = Cliente
+        }
+        Conta.Depositar(Double.Parse(VetorLinha(2)) - 100)
+
+        Return Conta
+
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Using FLuxoDeArquivo As New FileStream("ContasExternas.csv", FileMode.Create)
+            Dim ContaString As String = "456;78945;4785,9;221122323;Gustavo Santos"
+            Dim UTF As New UTF8Encoding()
+            Dim Bytes = UTF.GetBytes(ContaString)
+            FLuxoDeArquivo.Write(Bytes, 0, Bytes.Count)
+        End Using
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Using FLuxoDeArquivo As New FileStream("ContasExternas.csv", FileMode.Create)
+            Using Escritor As New StreamWriter(FLuxoDeArquivo, Encoding.UTF8)
+                Escritor.WriteLine("456;78945;4785,9;221122323;José da Silva")
+            End Using
+        End Using
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        TextBox2.Text = ""
+        Using FLuxoDeArquivo As New FileStream("Teste.txt", FileMode.Create)
+            Using Escritor As New StreamWriter(FLuxoDeArquivo, Encoding.UTF8)
+                For i As Integer = 0 To 1000
+                    TextBox2.AppendText($"Linha {i}{vbCrLf}")
+                    Escritor.WriteLine($"Linha {i}")
+                    Escritor.Flush()
+                Next
+            End Using
+        End Using
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        Using FLuxoDeArquivo As New FileStream("ContaCorrenteBinario.bin", FileMode.Create)
+            Using Escritor As New BinaryWriter(FLuxoDeArquivo)
+                Dim agencia As Integer = 234
+                Dim conta As Integer = 122221
+                Dim saldo As Double = 4323.23
+                Dim cpf As String = "12222111122"
+                Dim nome As String = "João da Silva"
+
+                Escritor.Write(agencia)
+                Escritor.Write(conta)
+                Escritor.Write(saldo)
+                Escritor.Write(cpf)
+                Escritor.Write(nome)
+            End Using
+        End Using
+
+        Using FLuxoDeArquivo As New FileStream("ContaCorrenteBinario.bin", FileMode.Open)
+            Using Leitor As New BinaryReader(FLuxoDeArquivo)
+                Dim agencia As Integer = Leitor.ReadInt32()
+                Dim conta As Integer = Leitor.ReadInt32()
+                Dim saldo As Double = Leitor.ReadDouble()
+                Dim cpf As String = Leitor.ReadString()
+                Dim nome As String = Leitor.ReadString()
+
+                MsgBox($"Agência: {agencia}, Conta: {conta}, Saldo: {saldo}, CPF: {cpf}, None: {nome}")
+            End Using
+        End Using
+
+    End Sub
+End Class
---
+++ VB.NET com Windows Forms Parte 10 - Entrada e Saída (IO) com Streams/ByteBank/ByteBank.SistemaAgencia/Frm_ManutencaoCCListView.vb
@@ -0,0 +1,164 @@
+﻿Imports System.IO
+Imports ByteBank.Bibliotecas.Classes.Clientes
+
+Public Class Frm_ManutencaoCCListView
+
+    Dim ListaContas As New List(Of ContaCorrente)
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+        Me.Text = "Manutenção Conta Corrente"
+        Grp_Lista.Text = "Lista de Contas Correntes"
+        Grp_CriterioOrdenacao.Text = "Critério de Ordenação"
+        Rb_Conta.Text = "Conta"
+        Rb_Nome.Text = "Nome"
+        Rb_Saldo.Text = "Saldo"
+
+        InicializacaoContas()
+        FormatarListView()
+        CriarColunas()
+        AtualizarListView()
+
+    End Sub
+
+    Sub InicializacaoContas()
+        ' Previmanete configurar algumas contas
+        'ListaContas.Add(New ContaCorrente(277, 144323, "João da Silva"))
+        'ListaContas.Add(New ContaCorrente(277, 155532, "Pedro Alcantara"))
+        'ListaContas.Add(New ContaCorrente(277, 233224, "Julia Mattos"))
+        'ListaContas.Add(New ContaCorrente(277, 983323, "Cláudia Organa"))
+        'ListaContas.Add(New ContaCorrente(277, 788233, "José Firmino"))
+        'ListaContas.Add(New ContaCorrente(277, 322352, "Julio de Castilho"))
+
+        Using fs As New FileStream("ListaContasCorrentes.txt", FileMode.Open)
+            Using sr As New StreamReader(fs)
+                While Not sr.EndOfStream
+                    Dim linha() As String = sr.ReadLine().Split(";")
+                    Dim conta As New ContaCorrente(Val(linha(0)), Val(linha(1)), linha(2))
+                    conta.Depositar(Double.Parse(linha(3)) - 100)
+                    ListaContas.Add(conta)
+                End While
+            End Using
+        End Using
+    End Sub
+
+    Sub FormatarListView()
+        ' Formatar o ListView
+
+        Lvw_Contas.View = View.Details
+        Lvw_Contas.LabelEdit = False
+        Lvw_Contas.AllowColumnReorder = False
+        Lvw_Contas.FullRowSelect = True
+        Lvw_Contas.GridLines = True
+
+    End Sub
+
+    Sub CriarColunas()
+        ' Configurar colunas
+
+        Lvw_Contas.Columns.Add("Conta Corrente", 100, HorizontalAlignment.Center)
+        Lvw_Contas.Columns.Add("Correntista", 150, HorizontalAlignment.Center)
+        Lvw_Contas.Columns.Add("Saldo em R$", 100, HorizontalAlignment.Center)
+    End Sub
+
+    Sub AtualizarListView()
+
+        ' Inclusao dos elementos
+
+        Lvw_Contas.Items.Clear()
+
+        For I As Integer = 0 To ListaContas.Count - 1
+            Dim vContaNumero As String = ListaContas(I).agencia.ToString +
+                "-" + ListaContas(I).numero.ToString
+            Dim Linha As New ListViewItem
+            Linha.Name = vContaNumero
+            Linha.Text = vContaNumero
+            Linha.SubItems.Add(ListaContas(I).titular.nome)
+            Linha.SubItems.Add(ListaContas(I).saldo.ToString)
+            Lvw_Contas.Items.Add(Linha)
+
+        Next
+
+    End Sub
+
+    Private Sub NovoToolStripButton_Click(sender As Object, e As EventArgs) Handles NovoToolStripButton.Click
+        Dim F As New Frm_ContaCorrente
+        F.ShowDialog()
+        If F.Retorno Then
+            ListaContas.Add(F.Conta)
+            AtualizarListView()
+        End If
+
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+
+        Dim ListaContasExcluir As New List(Of ContaCorrente)
+        For I As Integer = 0 To Lvw_Contas.Items.Count - 1
+            If Lvw_Contas.Items(I).Selected Then
+                Dim ContaExcluir As ContaCorrente
+                ContaExcluir = ListaContas(I)
+                ListaContasExcluir.Add(ContaExcluir)
+            End If
+        Next
+
+        If ListaContasExcluir.Count > 0 Then
+            For I As Integer = 0 To ListaContasExcluir.Count - 1
+                Dim ContaExcluir As ContaCorrente
+                ContaExcluir = ListaContasExcluir(I)
+                ListaContas.Remove(ContaExcluir)
+            Next
+            AtualizarListView()
+        End If
+
+    End Sub
+
+    Private Sub Rb_Conta_CheckedChanged(sender As Object, e As EventArgs) Handles Rb_Conta.CheckedChanged
+
+        Dim ListaAuxiliar As IEnumerable(Of ContaCorrente) = ListaContas _
+            .OrderBy(Function(conta As ContaCorrente) conta.agencia) _
+            .OrderBy(Function(conta As ContaCorrente) conta.numero)
+
+        AtualizaListaContas(ListaAuxiliar)
+
+    End Sub
+
+    Private Sub Rb_Nome_CheckedChanged(sender As Object, e As EventArgs) Handles Rb_Nome.CheckedChanged
+        Dim ListaAuxiliar As IEnumerable(Of ContaCorrente) = ListaContas _
+           .OrderBy(Function(conta As ContaCorrente) conta.titular.nome)
+
+        AtualizaListaContas(ListaAuxiliar)
+
+    End Sub
+
+    Private Sub Rb_Saldo_CheckedChanged(sender As Object, e As EventArgs) Handles Rb_Saldo.CheckedChanged
+        Dim ListaAuxiliar As IEnumerable(Of ContaCorrente) = ListaContas _
+          .OrderBy(Function(conta As ContaCorrente) conta.saldo)
+
+        AtualizaListaContas(ListaAuxiliar)
+    End Sub
+
+    Sub AtualizaListaContas(ListaAuxiliar As IEnumerable(Of ContaCorrente))
+        ListaContas = New List(Of ContaCorrente)
+
+        For I As Integer = 0 To ListaAuxiliar.Count - 1
+            ListaContas.Add(ListaAuxiliar(I))
+        Next
+        AtualizarListView()
+    End Sub
+
+    Private Sub SaveToolStripButton_Click(sender As Object, e As EventArgs) Handles SaveToolStripButton.Click
+        Using fs As New FileStream("ListaContasCorrentes.txt", FileMode.Create)
+            Using sw As New StreamWriter(fs)
+                For Each conta As ContaCorrente In ListaContas
+                    sw.WriteLine($"{conta.agencia};{conta.numero};{conta.titular.nome};{conta.saldo}")
+                Next
+            End Using
+        End Using
+        MsgBox("Dados salvos com sucesso.")
+    End Sub
+End Class
